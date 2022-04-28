2aa2425adcbf16bf84d4b8f7c4ddeb15c7033825
Jordan <jaycedam@gmail.com>
2022-04-27 20:25:07 -0400

    project component with props

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,7 +1,13 @@
 import React from "react";
 
-export default function Project() {
+export default function Project(props) {
     return (
-        <></>
+        <div className="feature col">
+            <h2>{props.name}</h2>
+            <p>{props.about}</p>
+            <a href={props.url} className="icon-link">
+                Link
+            </a>
+        </div>
     )
 }
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,6 +2,12 @@ import React from "react";
 
 export default function ProjectList() {
     return (
-        <></>
+        <div className="container px-4 py-5" id="projects">
+            <h2 className="pb-2 border-bottom">Proyectos</h2>
+
+            <div className="row g-4 py-5 row-cols-1 row-cols-lg-3">
+                <h3>Placeholder</h3>
+            </div>
+        </div>
     )
 }
