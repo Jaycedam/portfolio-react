3587082ad413451c248f11b96bed762d133acdfb
Jordan <jaycedam@gmail.com>
2022-04-27 21:41:16 -0400

    API for projects

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Project from "./Project"
 
 export default function ProjectList() {
+
+    const [projects, setProjects] = useState([]);
+    const url = "https://jordan-cortes-admin.herokuapp.com/api/v1/projects"
+
+    const fetchApi = async () => {
+        const response = await fetch(url)
+        const responseJSON = await response.json()
+        setProjects(responseJSON)
+        console.log(response.status)
+    }
+
+    useEffect(() => {
+        fetchApi()
+    }, [])
+
+    const projectElements = projects.map(n => {
+        return <Project key={n.id}
+            name={n.name}
+            about={n.about}
+            url={n.url}
+        />
+    }
+    );
+
     return (
         <div className="container px-4 py-5" id="projects">
             <h2 className="pb-2 border-bottom">Proyectos</h2>
 
             <div className="row g-4 py-5 row-cols-1 row-cols-lg-3">
-                <h3>Placeholder</h3>
+                {projectElements}
             </div>
         </div>
     )
