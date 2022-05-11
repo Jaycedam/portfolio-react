0ba509b4a79a43a2f964f889e10c861d86d69228
Jordan <jaycedam@gmail.com>
2022-05-11 11:28:45 -0400

    new logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import { AiOutlineClose } from "react-icons/ai";
 import './assets/css/navbar.css';
 import ThemeToggle from "../ThemeToggle/ThemeToggle"
 import { motion } from "framer-motion";
-import {MdEmail} from "react-icons/md";
+import { MdEmail } from "react-icons/md";
 import Button from "../Button/Button";
 
 // enamble/disable navbar on mobile when toggled
@@ -55,8 +55,8 @@ export default function Navbar() {
 
             {/* Mail btn on mobile */}
             <motion.div
-                className="mail-btn-mobile" 
-                initial={{ 
+                className="mail-btn-mobile"
+                initial={{
                     scale: 0,
                     opacity: 0
                 }}
@@ -64,19 +64,25 @@ export default function Navbar() {
                     scale: 1,
                     opacity: 1,
                     transition: {
-                        delay:3, 
+                        delay: 3,
                         type: "spring",
                     }
                 }}
             >
-                <a 
+                <a
                     href="mailto:jaycedam@gmail.com">{<MdEmail />}</a>
             </motion.div>
 
             <header className="primary-header" data-visible="false">
                 <div className="logo">
-                    {/* <img src={logo} alt="logo"/> */}
-                    <strong>LOGO</strong>
+                    <svg id="logo-svg" xmlns="http://www.w3.org/2000/svg" width="931.31" height="715.26" viewBox="0 0 931.31 715.26">
+                        <g id="letters">
+                            <path d="M559.24,398.12c0,29.37,0,65.9,11.93,94.21,12.53,31.28,39.49,47.24,80.11,47.24,38.77,0,66.05-17.26,81.06-51.28l0-.11.06-.13c8.2-20.11,11.16-42.84,12.24-63.15a9.61,9.61,0,0,1,9.62-9.09H921.69a9.61,9.61,0,0,1,9.62,9.79c-2,88-15.53,141.56-75,208-67,73.69-164.3,81.67-207,81.67-86.65,0-148.1-27.83-201-82.69-46.51-48.31-75.34-104.37-75.34-234.45l31.88-159.67" />
+                            <path d="M559.24,606.65V317.14c0-29.37,0-65.9,11.93-94.21,12.53-31.29,39.49-47.24,80.11-47.24,38.77,0,66.05,17.26,81.06,51.28l0,.11.06.13c8.2,20.11,11.16,42.84,12.24,63.15a9.6,9.6,0,0,0,9.62,9.08H921.69a9.6,9.6,0,0,0,9.62-9.78c-2-88-15.53-141.56-75-208C789.26,8,692,0,649.25,0c-86.65,0-148.1,27.82-201,82.69-46.51,48.3-75.34,104.37-75.34,234.45V543.08" />
+                            <path d="M558.89,416.32c0,130.08-29.34,167.94-75.85,216.25-52.88,54.86-114.33,82.69-201,82.69-42.71,0-140-8-207-81.67-59.51-66.43-73-120-75-208a9.62,9.62,0,0,1,9.62-9.79H177a9.6,9.6,0,0,1,9.61,9.09c1.09,20.31,4.05,43,12.24,63.15l.06.13,0,.11c15,34,42.29,51.28,81.07,51.28,40.61,0,67.58-16,80.11-47.24,11.92-28.31,12.94-46.64,12.94-76" />
+                        </g>
+                        <circle id="circle" cx="279.06" cy="88.9" r="88.37" fill="#ff0a52" />
+                    </svg>
                 </div>
 
                 <nav className="primary-navigation">
@@ -108,7 +114,7 @@ export default function Navbar() {
                                 text="Contactar"
                                 link="mailto:jaycedam@gmail.com"
                                 type="btn-color"
-                                icon={<MdEmail/>}
+                                icon={<MdEmail />}
                             />
                         </li>
 
